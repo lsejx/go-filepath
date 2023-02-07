5e8b7813b3426d71511a7c6edd8bc4271011c1fa
ab <ab@my-mac-0.local>
2023-02-07 18:51:36 +0900

    implemented

--- lib.go
+++ lib.go
@@ -1,1 +1,25 @@
 package filepath
+
+import "os"
+
+type Type int
+
+const (
+	NotFound  Type = 1 << iota
+	Directory Type = 1 << iota
+)
+
+func GetType(path string) Type {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return NotFound
+	}
+	if info.IsDir() {
+		return Directory
+	}
+	return 0
+}
+
+func IsExistingFile(t Type) bool {
+	return t&NotFound == 0 && t&Directory == 0
+}

47408769cfb029d18bb0e1782298ddf36fec1487
ab <ab@my-mac-0.local>
2023-02-07 18:54:23 +0900

    implemented IsNotExistingFile()

--- lib.go
+++ lib.go
@@ -23,3 +23,7 @@ func GetType(path string) Type {
 func IsExistingFile(t Type) bool {
 	return t&NotFound == 0 && t&Directory == 0
 }
+
+func IsNotExistingFile(t Type) bool {
+	return t&NotFound == NotFound && t&Directory == 0
+}

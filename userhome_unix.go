c703cea24319121a9835651fb286c82373c02b8e
ab <ab@my-mac-0.local>
2023-04-28 23:35:54 +0900

    update function description

--- userhome_unix.go
+++ userhome_unix.go
@@ -6,6 +6,7 @@ import (
 )
 
 // AbsHome replaces ~ (user home dir) with os.UserHomeDir().
+// If path doesn't has prefix "~", this does nothing.
 // Non-nil error is returned if os.UserHomeDir() returned non-nil error.
 func AbsHome(path string) (string, error) {
 	apply := path == "~" || strings.HasPrefix(path, "~/")

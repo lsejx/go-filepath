5289aa4f7ddd2b79a8f9a32c9d9b0a5002300d6b
ab <ab@my-mac-0.local>
2023-05-03 15:56:01 +0900

    rename variable

--- type_test.go
+++ type_test.go
@@ -3,7 +3,7 @@ package fpath
 import "testing"
 
 func TestGetType(t *testing.T) {
-	datas := []struct {
+	tests := []struct {
 		path          string
 		want          Type
 		isFile        bool
@@ -16,22 +16,22 @@ func TestGetType(t *testing.T) {
 		{".git", Type{true, true}, false, true, true, false},
 	}
 
-	for _, data := range datas {
-		tp := GetType(data.path)
-		if tp != data.want {
-			t.Fatalf("path:%v, want:%v, got:%v", data.path, data.want, tp)
+	for _, tt := range tests {
+		tp := GetType(tt.path)
+		if tp != tt.want {
+			t.Fatalf("path:%v, want:%v, got:%v", tt.path, tt.want, tp)
 		}
-		if tp.IsFile() != data.isFile {
-			t.Fatalf("tp.IsFile():%v, isFile:%v", tp.IsFile(), data.isFile)
+		if tp.IsFile() != tt.isFile {
+			t.Fatalf("tp.IsFile():%v, isFile:%v", tp.IsFile(), tt.isFile)
 		}
-		if tp.IsDir() != data.isDir {
-			t.Fatalf("tp.IsDir():%v, data.isDir:%v", tp.IsDir(), data.isDir)
+		if tp.IsDir() != tt.isDir {
+			t.Fatalf("tp.IsDir():%v, data.isDir:%v", tp.IsDir(), tt.isDir)
 		}
-		if tp.IsExisting() != data.isExisting {
-			t.Fatalf("tp.IsExisting():%v, data.isExisting:%v", tp.IsExisting(), data.isExisting)
+		if tp.IsExisting() != tt.isExisting {
+			t.Fatalf("tp.IsExisting():%v, data.isExisting:%v", tp.IsExisting(), tt.isExisting)
 		}
-		if tp.IsNotExisting() != data.isNotExisting {
-			t.Fatalf("tp.IsNotExisting():%v, data.isNotExisting:%v", tp.IsNotExisting(), data.isNotExisting)
+		if tp.IsNotExisting() != tt.isNotExisting {
+			t.Fatalf("tp.IsNotExisting():%v, data.isNotExisting:%v", tp.IsNotExisting(), tt.isNotExisting)
 		}
 	}
 }
